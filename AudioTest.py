71b776a0e049cd004fa8d9eea561ad6bf63e19c5
Fisher <ffugler@mymail.mines.edu>
2018-11-26 19:02:43 -0700

    ayyy its fixed

--- AudioTest.py
+++ AudioTest.py
@@ -4,17 +4,19 @@ import numpy as np
 import pylab as pl
 import wave
 
-file = wave.open("testSounds/92002__jcveliz__violin-origional.wav", mode="rb")
-print(file.getnchannels())
-print(file.getnframes())
-song = []
-for x in file.readframes(50864):
-    song.append(x)
-#print(song)
-x = [x for x in range(193)]
 
-print(type(song[1]))
-pl.plot(song[38833:39026], 'r.')
+file = wave.open("testSounds/92002__jcveliz__violin-origional.wav", mode="rb")
+#print(file.getnchannels()) 
+#print(file.getnframes())
+song = []
+bytelist = file.readframes(50896)
+
+#print(int.from_bytes(ioobject.read(2), 'little', signed = True))
+for x in range(0, len(bytelist),2):
+    song.append(int.from_bytes(bytelist[x:x+2], 'little', signed = True))
+
+
+pl.plot(song[38833:39026], 'r')
 pl.xlabel("X")
 pl.ylabel("Y")
 pl.title("Title")
